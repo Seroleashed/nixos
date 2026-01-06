064c62da368c4db12aa00d41e48085a7e0673980
Dustin Lorenz <dsilorenz@gmail.com>
2026-01-06 12:26:57 +0100

    Enhance sops.nix with example secrets comments
    Added comments for example secrets in sops configuration.

--- sops.nix
+++ sops.nix
@@ -1,8 +1,16 @@
-{ config, lib, ... }:
-
-{
-  sops = {
-    defaultSopsFile = ./secrets/secrets.yaml;
-    age.sshKeyPaths = [ "/home/stinooo/.ssh/sops-key" ];
-  };
-}
+# sops-nix Secrets Management
+sops = {
+  defaultSopsFile = ./secrets/secrets.yaml;
+  age.sshKeyPaths = [ "/home/stinooo/.ssh/sops-key" ];
+   
+  # Beispiel-Secrets (aktiviere nach Bedarf)
+  # secrets = {
+  #   wifi_password = {
+  #     owner = "root";
+  #   };
+  #   github_ssh_key = {
+  #     owner = "stinooo";
+  #     mode = "0600";
+  #   };
+  # };
+};
