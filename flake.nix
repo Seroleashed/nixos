135f853e473798ecb83e7033df23babe350ce3fe
Dustin Lorenz <dsilorenz@gmail.com>
2026-01-06 10:38:35 +0100

    Add NixOS configuration with device types and Home Manager
    This file defines a NixOS configuration with multiple device types including VMware, VirtualBox, laptop, and desktop. It integrates Home Manager for user-specific package management.

---
+++ flake.nix
@@ -0,0 +1,86 @@
+{
+  description = "NixOS Configuration with Device Types and Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # Home Manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations = {
+      # VMware VM
+      vmware = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          { device.type = "vmware"; }
+          
+          # Home Manager Integration
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.stinooo = import ./home.nix;
+          }
+        ];
+      };
+      
+      # VirtualBox VM
+      virtualbox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./configuration.nix
+          { device.type = "virtualbox"; }
+          
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.stinooo = import ./home.nix;
+          }
+        ];
+      };
+      
+      # Laptop
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration-laptop.nix
+          ./configuration.nix
+          { device.type = "laptop"; }
+          
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.stinooo = import ./home.nix;
+          }
+        ];
+      };
+      
+      # Desktop PC
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration-desktop.nix
+          ./configuration.nix
+          { device.type = "desktop"; }
+          
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.stinooo = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
