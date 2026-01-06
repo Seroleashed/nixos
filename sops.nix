85015cf2c225a29f8fd10c41293b50c6f548b735
Dustin Lorenz <dsilorenz@gmail.com>
2026-01-06 13:07:14 +0100

    Refactor sops.nix structure and comments

--- sops.nix
+++ sops.nix
@@ -1,16 +1,20 @@
+{ condig, lib, ... }:
+
 # sops-nix Secrets Management
-sops = {
-  defaultSopsFile = ./secrets/secrets.yaml;
-  age.sshKeyPaths = [ "/home/stinooo/.ssh/sops-key" ];
+{
+  sops = {
+    defaultSopsFile = ./secrets/secrets.yaml;
+    age.sshKeyPaths = [ "/home/stinooo/.ssh/sops-key" ];
    
-  # Beispiel-Secrets (aktiviere nach Bedarf)
-  # secrets = {
-  #   wifi_password = {
-  #     owner = "root";
-  #   };
-  #   github_ssh_key = {
-  #     owner = "stinooo";
-  #     mode = "0600";
-  #   };
-  # };
-};
+    # Beispiel-Secrets (aktiviere nach Bedarf)
+    # secrets = {
+    #   wifi_password = {
+    #     owner = "root";
+    #   };
+    #   github_ssh_key = {
+    #     owner = "stinooo";
+    #     mode = "0600";
+    #   };
+    # };
+  };
+}
