d58e9e13565f767740319fdb21066756437eed77
Dustin Lorenz <dsilorenz@gmail.com>
2026-01-08 14:21:02 +0100

    Update packages.nix

--- packages.nix
+++ packages.nix
@@ -1,33 +1,36 @@
-{ config, pkgs, ... }:
-
 {
+  config,
+  pkgs,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     # Terminal und Shell
     zsh
     tmux
-    
+
     # Shell-Tools
     starship
     zoxide
     pay-respects
     fzf
-    ripgrep  # Besseres grep, wird von fzf verwendet
-    bat      # Besseres cat mit Syntax-Highlighting
-    eza      # Besseres ls
-    fd       # Besseres find
-    
+    ripgrep # Besseres grep, wird von fzf verwendet
+    bat # Besseres cat mit Syntax-Highlighting
+    eza # Besseres ls
+    fd # Besseres find
+    navi # cheatsheet for commands
+
     # Entwicklung
     git
     vscode
-    
+
     # Remote-Tools
     rustdesk
     tailscale
-    
+
     # System-Tools
     htop
     neofetch
-    
+
     # Hilfreich für Debugging
     wayland-utils
     xwayland
