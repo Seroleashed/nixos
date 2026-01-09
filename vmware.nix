d9f566cd5acb769b2726ce59a246a688b3c0d7cb
Seroleashed <dsilorenz@gmail.com>
2026-01-09 09:32:38 +0100

    vmware-Anpassungen für bidirektionales Copy Paste

--- vmware.nix
+++ vmware.nix
@@ -5,19 +5,24 @@
 {
   # VMware Guest Tools - Dies aktiviert automatisch open-vm-tools
   virtualisation.vmware.guest.enable = true;
-  
+
   # Video Driver
-  services.xserver.videoDrivers = [ "vmware" ];
-  
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "vmware" ];
+    modules = [ pkgs.xorg.xf86inputvmouse ];
+  };
+
   # Open VM Tools explizit installieren (für manuelle Tools)
   environment.systemPackages = with pkgs; [
     open-vm-tools
+    gtkmm3
   ];
-  
+
   # Services für Copy/Paste und Integration
   # Diese werden durch virtualisation.vmware.guest.enable automatisch gestartet,
   # aber wir definieren sie explizit für bessere Kontrolle
-  
+
   systemd.services.vmtoolsd = {
     description = "VMware Tools Daemon";
     wantedBy = [ "multi-user.target" ];
@@ -43,22 +48,33 @@
       RestartSec = "2";
     };
   };
-  
+
+  systemd.services.vmware-vmblock-fuse = {
+    description = "VMware VMBLOCK fuse";
+    after = [ "multi-user.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.open-vm-tools}/bin/vmware-vmblock-fuse -o subtype=vmware-vmblock,default_permissions,allow_other /run/vmblock-fuse";
+      ExecStop = "${pkgs.utillinux}/bin/umount -l /run/vmblock-fuse";
+      Restart = "always";
+    };
+  };
+
   # VMCI (Virtual Machine Communication Interface) für bessere Integration
   boot.kernelModules = [ "vmw_vsock_vmci_transport" "vmw_vmci" "vmwgfx" ];
-  
+
   # Shared Folders Support (optional, auskommentiert)
   # Aktiviere dies wenn du Shared Folders zwischen Host und Guest nutzen möchtest:
-  # 
+  #
   # boot.kernelModules = [ "vmhgfs" ];
-  # 
+  #
   # systemd.mounts = [{
   #   what = ".host:/";
   #   where = "/mnt/hgfs";
   #   type = "fuse.vmhgfs-fuse";
   #   options = "allow_other,auto_unmount";
   # }];
-  # 
+  #
   # systemd.automounts = [{
   #   where = "/mnt/hgfs";
   #   wantedBy = [ "multi-user.target" ];
